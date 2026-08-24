fa0d016ece666d8785026e6626ad704f7940f64a
Sergey <spect420@bk.ru>
2026-08-24 17:59:48 +0300

    predict.py v.1

---
+++ src/predict.py
@@ -0,0 +1,25 @@
+# predict.py
+
+from src.config import PROJECT_PATH
+import joblib
+import pandas as pd
+
+
+def load_artifact(
+        path = PROJECT_PATH / "models" / "catboost_fraud_model.joblib"
+):
+    return joblib.load(path)
+
+def predict(X, artifact):
+    model = artifact['model']
+    threshold = artifact['threshold']
+
+    probabilities = model.predict_proba(X)[:, -1]
+    predictions = (probabilities >= threshold).astype(int)
+
+    result = pd.DataFrame({
+        "probabilities": probabilities,
+        "predictions": predictions
+    })
+
+    return result
