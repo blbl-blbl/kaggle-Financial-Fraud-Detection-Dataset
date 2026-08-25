1d32b8a5cec03d1a2b3cec4df8e8bdc3ac9db149
Sergey <spect420@bk.ru>
2026-08-25 18:33:52 +0300

    func predict_raw added

--- src/predict.py
+++ src/predict.py
@@ -4,6 +4,8 @@ from src.config import PROJECT_PATH
 import joblib
 import pandas as pd
 
+from src.features import build_features
+
 
 def predict(X, artifact):
 
@@ -19,3 +21,11 @@ def predict(X, artifact):
     })
 
     return result
+
+
+def predict_raw(df, artifact):
+    df = build_features(df)
+
+    X = df[artifact["features"]]
+
+    return predict(X, artifact)
