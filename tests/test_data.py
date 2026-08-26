94f9499be57a947c9a5d2da8cc6fd115a326744e
Sergey <spect420@bk.ru>
2026-08-26 15:44:54 +0300

    test_validate_columns_valid_dataframe added

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,11 @@
 # test_data.py
+from unittest import result
 
 import pandas as pd
 import pytest
 from src.data import validate_columns
+from src.config import BASE_FEATURES, TARGET
+
 
 def test_validate_columns_raises_error():
     df = pd.DataFrame({
@@ -12,3 +15,15 @@ def test_validate_columns_raises_error():
 
     with pytest.raises(ValueError):
         validate_columns(df)
+
+
+def test_validate_columns_valid_dataframe():
+    required_columns = BASE_FEATURES + [TARGET]
+
+    df = pd.DataFrame({
+        column: [0, 1] for column in required_columns
+    })
+    result = validate_columns(df)
+
+    assert result.columns.tolist() == required_columns
+    assert len(result) == 2
